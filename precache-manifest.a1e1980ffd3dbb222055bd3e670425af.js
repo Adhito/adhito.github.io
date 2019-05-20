3c8392103b0037491074541f3f64548b1bcf8a86
Adhito <adhito909@gmail.com>
2019-05-20 23:00:38 +0700

    Updates

---
+++ precache-manifest.a1e1980ffd3dbb222055bd3e670425af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01447f7b9523bf6366a3",
+    "url": "/static/js/main.01447f7b.chunk.js"
+  },
+  {
+    "revision": "bda1397368721c5f3654",
+    "url": "/static/js/1.bda13973.chunk.js"
+  },
+  {
+    "revision": "01447f7b9523bf6366a3",
+    "url": "/static/css/main.a1639080.chunk.css"
+  },
+  {
+    "revision": "bda1397368721c5f3654",
+    "url": "/static/css/1.9199f798.chunk.css"
+  },
+  {
+    "revision": "c03105deec286d00e204b24becb7ed02",
+    "url": "/index.html"
+  }
+];
